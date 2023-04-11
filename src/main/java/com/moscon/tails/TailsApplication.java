a070e70701210eda4dc4181ad1c60dad92cd48d1
Emiliano G. Garello <emiliano.garello@gmail.com>
2023-04-11 17:40:33 -0300

    first commit

---
+++ src/main/java/com/moscon/tails/TailsApplication.java
@@ -0,0 +1,13 @@
+package com.moscon.tails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moscon/tails/TailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moscon.tails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
